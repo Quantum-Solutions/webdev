0951dbcaf9a57b635f02f66f1bb5a732908c2986
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-11-21 20:43:44 +0200

    module css 2

---
+++ js/user/marks.js
@@ -0,0 +1,39 @@
+$(document).ready(async function() {
+    try {
+        const id = UserAuthHelper.getUserId();
+        const response = await api.getMarks(id);
+
+        if(response.ok){
+            const table = `
+            <table border="1">
+              <thead>
+                <th>Module Name</th>
+                <th>Module Code</th>
+              </thead>
+              <tbody>
+                ${response.data.map(v => {
+                  return `
+                    <tr>
+                      <td>${v.name}</td>
+                      <td>${v.code}</td>
+                    </tr>
+                  `;
+                })}
+              </tbody>
+            </table>
+          `;
+          
+          const div = $('#moduleInfo');
+          div.append(table);
+  
+          
+        }
+        else{
+            console.log('Not working')
+        }
+
+        $('#spinner').hide();
+    } catch (error) {
+        console.log(error);
+    }
+});
