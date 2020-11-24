7d3ed0da04a48ae7d13497195960b4875105e199
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-11-24 20:37:04 +0200

    lecDetails staged

---
+++ js/user/lect-details.js
@@ -0,0 +1,26 @@
+$(document).ready(async function() {
+    try {
+        const id = UserAuthHelper.getUserId();
+        const response = await api.getUser(id);
+
+        if(response.ok){
+            
+            const { firstName, email } = response.data;
+            console.log(response.data);
+            console.log( $('#firstName'));
+            $('#firstName').val(firstName);
+            $('#email').val(email);
+
+
+            $('#loadStatus').hide();
+            $('.profSection').show();
+        }
+        else{
+            console.log('Not working')
+        }
+
+        $('#spinner').hide();
+    } catch (error) {
+        console.log(error);
+    }
+});
