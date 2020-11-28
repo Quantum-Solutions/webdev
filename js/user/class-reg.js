b4cc3415bc384683f603e50f7adb4e3e0ad7a0d3
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-11-28 15:48:25 +0200

    Testing lec-reg

--- js/user/class-reg.js
+++ js/user/class-reg.js
@@ -4,7 +4,31 @@ $(document).ready(async function() {
         const id = UserAuthHelper.getUserId();
         const response = await api.getClassRegister(id);
         
-         if(response.ok){
+        if(response.ok){
+        //    response = JSON.parse(`{
+        //     "ok": true,
+        //     "data": [
+        //         {
+        //             "username": "179258421",
+        //             "firstName": "Keegan",
+        //             "lastName": "Launspach",
+        //             "attended": true
+        //         },
+        //         {
+        //             "username": "183005472",
+        //             "firstName": "Jared",
+        //             "lastName": "Govender",
+        //             "attended": true
+        //         },
+        //         {
+        //             "username": "170845789",
+        //             "firstName": "Aaron",
+        //             "lastName": "Garton",
+        //             "attended": true
+        //         }
+        //     ]
+        // }`);
+
             const table = `
             <table border="1">
               <thead>
