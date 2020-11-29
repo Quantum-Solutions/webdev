187a1dbe0a09d88647a2c859154dc827ca4f5e2d
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-11-29 14:13:55 +0200

    Updateprofile

--- js/user/user-profile.js
+++ js/user/user-profile.js
@@ -38,29 +38,56 @@ $(document).ready(async function() {
 //PUT Request
 $('#btnUpdateDetails').click(function() {
     const id = UserAuthHelper.getUserId();
-    const response = api.putProfile(id);
-    const { firstName, lastName, contactNumber, address, gender, email} = response.data;
+    
+    const { firstName, lastName, contact_number, address, gender, email} = response.data;
    
 
-    if(response.ok){
+    try{
       
-        console.log(push.data);
-  
-        $(firstName).val('#firstName');
-        $('#lastName').val(lastName);
-        $('#contactNo').val(contactNumber);
-        $('#address').val(address);
-        $('#gender').val(gender);
+        //console.log(push.data);
         
-        //Make 2nd API Call for username and course
-        // .value for inputs and .text for labels
-        $('#course').text(course.code);
-        $('#studentNumber').text(username);
-        $('#currentEmail').val(email);
+        const firstName = $('#firstName').val();
+        const lastName = $('#lastName').val();
+        const email = $('#email').val();
+        const contact_number = $('#contactNo').val();
+        const address = $('#address').val();
+        const gender = $('#gender').val();
+
+        const response = api.putProfile(id);
         console.log('PUT REQUEST ATTEMPTED!')
        
     }
-    else{
+    catch{
         console.log('PUT Not working')
     }
+
+
+    const credentials = { firstName, lastName, email, password, passwordConf, id_number, contact_number, address, gender, city, 
+        province, zipCode, country, highschool,engMark, fistLangMark, mathMark, scienceMark, extra1Mark, extra2Mark,
+      course };
+      
+      try {
+        const response = await auth.register(credentials);
+        console.log(response);
+    
+       
+    
+          if (response.ok) {
+    
+            window.location.href = 'index.html';
+            
+          } 
+          else {
+    
+            $('#message').text(response.message);
+    
+          }
+          console.log(response);
+    
+      } catch (err) {
+    
+        console.log(err);
+        $('#message').text('Invalid email/password');
+        console.log('fail');
+      }  
 });
