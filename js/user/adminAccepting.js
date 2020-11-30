47a5cc8388127976bafbd5c3300b1eec650ab7c0
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-11-30 22:43:16 +0200

    adminPagenew

---
+++ js/user/adminAccepting.js
@@ -0,0 +1,43 @@
+$(document).ready(async function() {
+    try {
+        const id = UserAuthHelper.getUserId();
+        const response = await api.getMarks(id);
+
+        if(response.ok){
+            const table = `
+            <table border="1">
+              <thead>
+                <th>Module Code</th>
+                <th>Mark(%) </th>
+              </thead>
+              <tbody>
+                ${response.data.map(v => {
+                  return `
+                    <tr>
+                      <td>${v.module.code}</td>
+                      <td>${v.mark}</td>
+                      <td><button></button></td>
+                    </tr>
+                  `;
+                })}
+              </tbody>
+            </table>
+          `;
+          
+          const div = $('#moduleInfo');
+          
+          div.append(table);
+
+          $('#loadStatus').hide();
+          $('#moduleContainer').show();
+          
+        }
+        else{
+            console.log('Not working')
+        }
+
+        $('#spinner').hide();
+    } catch (error) {
+        console.log(error);
+    }
+});
