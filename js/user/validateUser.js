52ba7ac969905559b0e56a37570892ecc93cb5bf
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-12-01 11:23:45 +0200

    Title Changes fixed, posts navbar fix

--- js/user/validateUser.js
+++ js/user/validateUser.js
@@ -1,36 +1,51 @@
 function validateLect() {
-    // const userType = UserAuthHelper.getUserType();
-    // if(userType != "lecturer"){
+    const userType = UserAuthHelper.getUserType();
+    if(userType != "lecturer"){
   
-    //   window.location.href = 'index.html';
+      window.location.href = 'index.html';
   
-    // }
-    // else{
-    //   console.log("Authorized Lecturer.")
-    // }
+    }
+    else{
+      console.log("Authorized Lecturer.")
+    }
+  }
+
+  function validateLectBtn() {
+    const btn = document.getElementById("createPostBtn");
+    const div = document.getElementById("createPost");
+    const userType = UserAuthHelper.getUserType();
+    if(userType != "lecturer"){
+  
+      //Disable Button and Hide
+      btn.disabled = false;
+      div.style.display = "none";
+    }
+    else{
+      console.log("Authorized Lecturer.")
+    }
   }
 
   function validateStud() {
-    // const userType = UserAuthHelper.getUserType();
-    // if(userType != "student"){
+    const userType = UserAuthHelper.getUserType();
+    if(userType != "student"){
   
-    //   window.location.href = 'index.html';
+      window.location.href = 'index.html';
   
-    // }
-    // else{
-    //   console.log("Authorized Student.")
-    // }
+    }
+    else{
+      console.log("Authorized Student.")
+    }
   }
 
   function validateAdmin() {
-    // const userType = UserAuthHelper.getUserType();
-    // if(userType != "admin"){
+    const userType = UserAuthHelper.getUserType();
+    if(userType != "admin"){
   
-    //   window.location.href = 'index.html';
+      window.location.href = 'index.html';
   
-    // }
-    // else{
-    //   console.log("Authorized Admin.")
-    // }
+    }
+    else{
+      console.log("Authorized Admin.")
+    }
   }
   
