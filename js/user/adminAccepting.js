d0f19307d55f75bd6fe33e1a2dfc4056dcf574fc
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-12-01 11:34:14 +0200

    admin.js

--- js/user/adminAccepting.js
+++ js/user/adminAccepting.js
@@ -1,22 +1,23 @@
 $(document).ready(async function() {
     try {
         const id = UserAuthHelper.getUserId();
-        const response = await api.getMarks(id);
+        const response = await api.getNotAccepted();
 
         if(response.ok){
             const table = `
             <table border="1">
               <thead>
-                <th>Module Code</th>
-                <th>Mark(%) </th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>isAccepted</th>
               </thead>
               <tbody>
                 ${response.data.map(v => {
                   return `
                     <tr>
-                      <td>${v.module.code}</td>
-                      <td>${v.mark}</td>
-                      <td><button></button></td>
+                      <td>${v.name}</td>
+                      <td>${v.email}</td>
+                      <td><button>Accept</button></td>
                     </tr>
                   `;
                 })}
@@ -24,12 +25,12 @@ $(document).ready(async function() {
             </table>
           `;
           
-          const div = $('#moduleInfo');
-          
+          const div = $('#userData');
           div.append(table);
+          console.log(response);
 
           $('#loadStatus').hide();
-          $('#moduleContainer').show();
+          $('#container').show();
           
         }
         else{
